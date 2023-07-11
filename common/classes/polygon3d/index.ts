73be3cdb917a5a6fadef9ea3165751f3c3fc6dfd
LAOVA <1723263863@qq.com>
2023-07-11 17:11:12 +0800

    优化格式

--- common/classes/polygon3d/index.ts
+++ common/classes/polygon3d/index.ts
@@ -66,352 +66,193 @@ export interface IPolygon3d {
 
 
     /**
-     * clear(fn)
-    删除场景中所有的3DPolygon
-
-    Parameters:
-    Name	Type	Description
-
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+     * 删除场景中所有的3DPolygon
+     * @param fn 可选的回调函数
      */
     clear(fn?: Function): Promise<BaseResult>
 
 
     /**
-     * delete(ids, fn)
-    删除一个或多个3DPolygon对象
-
-    Parameters:
-    Name	Type	Description
-
-    ids	string | array	
-    要删除的3DPolygon对象的ID或者ID数组（可以删除一个或者多个）
-
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+     * 删除一个或多个3DPolygon对象
+     * 
+     * @param ids 要删除的标注对象的ID或者ID数组（可以删除一个或者多个）
+     * @param fn 可选的回调函数
      */
     delete(ids: string | string[], fn?: Function): Promise<BaseResult>
 
 
     /**
-     * focus(ids, distance, flyTime, rotation, fn)
-    自动定位到合适的观察距离
-
-    Parameters:
-    Name	Type	Description
-
-    ids	string | array	
-    3DPolygon对象的ID或者ID数组
-
-    distance	number	
-    可选参数，观察点距离目标点（被拍摄物体）的距离，取值范围：[0.01~任意正数]，如果设置为0或者不设置，系统自动计算
-
-    flyTime	number	
-    可选参数，相机飞行的时间，取值范围：[0~任意正数]，单位：秒，默认值2秒
-
-    rotation	array	
-    可选参数，相机旋转的欧拉角：[Pitch,Yaw,Roll]，数组元素类型：(number)，取值范围：Pitch[-90~90] Yaw[-180~180] Roll[0]
-
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+     * 自动定位到合适的观察距离
+     * 
+     * @param ids 3DPolygon对象的ID或者ID数组
+     * @param distance	可选参数，观察点距离目标点（被拍摄物体）的距离，取值范围：[0.01~任意正数]，如果设置为0或者不设置，系统自动计算
+     * @param flyTime	可选参数，相机飞行的时间，取值范围：[0~任意正数]，单位：秒，默认值2秒
+     * @param rotation	可选参数，相机旋转的欧拉角：[Pitch,Yaw,Roll]，数组元素类型：(number)，取值范围：Pitch[-90~90] Yaw[-180~180] Roll[0]
+     * @param fn  可选的回调函数，请参考二次开发：异步接口调用方式
      */
     focus(ids: string | string[], distance?: number, flyTime?: number, rotation?: number[], fn?: Function): Promise<BaseResult>
 
 
     /**
-     * get(ids, fn)
-    根据ID获取3DPolygon的详细信息
-
-    Parameters:
-    Name	Type	Description
-
-    ids	string | array	
-    要获取的3DPolygon对象ID或者ID数组（可以获取一个或者多个）
-
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+     * 根据ID获取3DPolygon的详细信息
+     * 
+     * @param ids 要获取的3DPolygon对象ID或者ID数组（可以获取一个或者多个）
+     * @param fn 可选的回调函数，请参考二次开发：异步接口调用方式
      */
     get(ids: string | string[], fn?: Function): Promise<BaseResult>
 
 
     /**
-     * glow(ids, duration, fn)
-    闪烁，目前仅部分样式支持闪烁，和材质有关
-
-    Parameters:
-    Name	Type	Description
-
-    ids	string | array	
-    Polygon3D的ID或者ID数组
-
-    duration	number	
-    闪烁持续时间，取值范围：[0~1]，单位：秒
-
-    fn	function	
-可选的回调函数，请参考二次开发：异步接口调用方式
+     * 闪烁，目前仅部分样式支持闪烁，和材质有关
+     * 
+     * @param ids	Polygon3D的ID或者ID数组
+     * @param duration	闪烁持续时间，取值范围：[0~1]，单位：秒
+     * @param fn  可选的回调函数，请参考二次开发：异步接口调用方式
      */
     glow(ids: string | string[], duration?: number, fn?: Function): Promise<BaseResult>
 
 
     /**
-     * hide(ids, fn)
-    隐藏3DPolygon
-
-    Parameters:
-    Name	Type	Description
-
-    ids	string | array	
-    3DPolygon对象的ID或者ID数组
-
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+     * 隐藏3DPolygon
+     * 
+     * @param ids	3DPolygon对象的ID或者ID数组
+     * @param fn	可选的回调函数，请参考二次开发：异步接口调用方式
      */
     hide(ids: string | string[], fn?: Function): Promise<BaseResult>
 
 
     /**
-     * hideAll(fn)
-    隐藏所有3DPolygon
-
-    Parameters:
-    Name	Type	Description
-
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+     * 隐藏所有3DPolygon
+     * 
+     * @param fn	可选的回调函数，请参考二次开发：异步接口调用方式
      */
     hideAll(fn?: Function): Promise<BaseResult>
 
 
     /**
-     * highlight(ids, fn)
-    高亮，目前仅部分样式支持高亮，和材质有关
-
-    Parameters:
-    Name	Type	Description
-
-    ids	string | array	
-    Polygon3D的ID或者ID数组
-
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+     * 高亮，目前仅部分样式支持高亮，和材质有关
+     * 
+     * @param ids	3DPolygon对象的ID或者ID数组
+     * @param fn	可选的回调函数，请参考二次开发：异步接口调用方式
      */
     highlight(ids: string | string[], fn?: Function): Promise<BaseResult>
 
 
     /**
-    * setColor(id, newColor, fn)
-    设置颜色
-
-    Parameters:
-    Name	Type	Description
-
-    id	string	
-    Polygon3D的ID
-
-    newColor	Color	
-    新颜色值，支持四种格式，取值示例
-
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+    * 设置颜色
+    * 
+    * @param ids	3DPolygon对象的ID
+    * @param newColor 新颜色值，支持四种格式，取值示例
+    * @param fn	可选的回调函数，请参考二次开发：异步接口调用方式
      */
     setColor(id: string, newColor: EColor, fn?: Function): Promise<BaseResult>
 
 
     /**
-     * setCoordinates(id, newVal, fn)
-     设置坐标
- 
-     Parameters:
-     Name	Type	Description
-
-     id	string	
-     Polygon3D的ID
- 
-     newVal	array	
-     新坐标值，取值示例
- 
-     fn	function	
-     可选的回调函数，请参考二次开发：异步接口调用方式
+     *设置坐标
+    * @param ids	3DPolygon对象的ID
+    * @param newVal  新坐标值，取值示例
+    * @param newColor 新颜色值，支持四种格式，取值示例
      */
     setCoordinates(id: string, newVal: number[], fn?: Function): Promise<BaseResult>
 
 
     /**
-     * setHeight(id, newVal, fn)
-    设置高度
-
-    Parameters:
-    Name	Type	Description
-
-    id	string	
-    Polygon3D的ID
-
-    newVal	number	
-    新值
-
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+     * 设置高度
+     * 
+     * @param id	Polygon3D的ID
+     * @param newVal	新值
+     * @param fn	可选的回调函数，请参考二次开发：异步接口调用方式
      */
     setHeight(id: string, newVal: number, fn?: Function): Promise<BaseResult>
 
 
     /**
-     * setIntensity(id, newVal, fn)
-     设置亮度
- 
-     Parameters:
-     Name	Type	Description
-
-     id	string	
-     Polygon3D的ID
- 
-     newVal	number	
-     新值
- 
-     fn	function	
-     可选的回调函数，请参考二次开发：异步接口调用方式
+     * 设置亮度
+     * 
+    * @param ids	3DPolygon对象的ID
+    * @param newVal  新值
+    * @param newColor 新颜色值，支持四种格式，取值示例
      */
     setIntensity(id: string, newVal: number, fn?: Function): Promise<BaseResult>
 
 
     /**
-     * setStyle(id, newVal, fn)
-    设置样式
-
-    Parameters:
-    Name	Type	Description
-
-    id	string	
-    Polygon3D的ID
-
-    newVal	number	
-    新3DPolygon的样式，参考 Polygon3DStyle
-
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+     * 设置样式
+     * 
+     * @param id	Polygon3D的ID
+     * @param  newVal	新3DPolygon的样式，参考 Polygon3DStyle
+     * @param fn	可选的回调函数，请参考二次开发：异步接口调用方式
      */
     setStyle(id: string, newVal: EPolygon3DStyle, fn?: Function): Promise<BaseResult>
 
 
     /**
-     * setTillingX(id, newVal, fn)
-    设置TillingX
-
-    Parameters:
-    Name	Type	Description
-
-    id	string	
-    Polygon3D的ID
-
-    newVal	number	
-    新值
-
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+     * 设置TillingX
+     * 
+     *  @param  id	Polygon3D的ID
+     *  @param newVal	新值
+     *  @param fn	可选的回调函数，请参考二次开发：异步接口调用方式
      */
     setTillingX(id: string, newVal: number, fn?: Function): Promise<BaseResult>
 
 
     /**
-     * setTillingY(id, newVal, fn)
-    设置TillingY
-
-    Parameters:
-    Name	Type	Description
-
-    id	string	
-    Polygon3D的ID
-
-    newVal	number	
-    新值
-
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+     * 设置TillingY
+     * 
+     * @param id	Polygon3D的ID
+     * @param newVal	新值
+     * @param fn	可选的回调函数，请参考二次开发：异步接口调用方式
      */
     setTillingY(id: string, newVal: number, fn?: Function): Promise<BaseResult>
 
 
     /**
-     * show(ids, fn)
-    显示3DPolygon
-
-    Parameters:
-    Name	Type	Description
-
-    ids	string | array	
-    3DPolygon对象的ID或者ID数组
-
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+     * 显示3DPolygon
+     * 
+     * @param ids	3DPolygon对象的ID或者ID数组
+     * @param fn	可选的回调函数，请参考二次开发：异步接口调用方式
      */
     show(ids: string | string[], fn?: Function): Promise<BaseResult>
 
 
     /**
-     * showAll(fn)
-    显示所有3DPolygon
-
-    Parameters:
-    Name	Type	Description
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+     * 显示所有3DPolygon
+     * 
+     * @param fn    可选的回调函数，请参考二次开发：异步接口调用方式
      */
     showAll(fn?: Function): Promise<BaseResult>
 
 
     /**
-     * stopGlow(ids, fn)
-    停止闪烁
-
-    Parameters:
-    Name	Type	Description
-
-    ids	string | array	
-    Polygon3D的ID或者ID数组
-
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+     * 停止闪烁
+     * 
+     * @param ids	Polygon3D的ID或者ID数组
+     * @param fn	可选的回调函数，请参考二次开发：异步接口调用方式
      */
     stopGlow(ids: string | string[], fn?: Function): Promise<BaseResult>
 
 
     /**
-     * stopHighlight(ids, fn)
-    停止高亮
-
-    Parameters:
-    Name	Type	Description
-
-    ids	string | array	
-    Polygon3D的ID或者ID数组
-
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+     * 停止高亮
+     * 
+     * @param ids	Polygon3D的ID或者ID数组
+     * @param fn	可选的回调函数，请参考二次开发：异步接口调用方式
      */
     stopHighlight(ids: string | string[], fn?: Function): Promise<BaseResult>
 
 
     /**
-     * update(data, fn)
-    修改一个或多个3DPolygon对象
-
-    Parameters:
-    Name	Type	Description
-
-    data	object | array	
-    数据结构，请参考add方法
-
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+     * 修改一个或多个3DPolygon对象
+     * 
+     * @param data	数据结构，请参考add方法
+     * @param fn	可选的回调函数，请参考二次开发：异步接口调用方式
      */
     update(data: IPolygon3dOptions | IPolygon3dOptions[], fn?: Function): Promise<BaseResult>
 
 
     /**
-     * updateBegin()
-    用于批量多次修改对象的属性
-
+     * 用于批量多次修改对象的属性
     在开始修改之前调用updateBegin，然后可以多次调用setXXX方法，最后调用updateEnd提交修改更新数据
     注意：
     updateBegin不是异步调用，不需要await，也没有回调函数参数
@@ -430,16 +271,9 @@ export interface IPolygon3d {
 
 
     /**
-     * updateEnd(fn)
-    用于批量多次修改对象的属性，与updateBegin配套使用
-    注意：
-    updateEnd是异步调用，可以用回调函数也可以await
-
-    Parameters:
-    Name	Type	Description
-    
-    fn	function	
-    可选的回调函数，请参考二次开发：异步接口调用方式
+     * 用于批量多次修改对象的属性，与updateBegin配套使用
+     * 
+     * @param  fn	可选的回调函数，请参考二次开发：异步接口调用方式
      */
     updateEnd(fn?: Function): Promise<BaseResult>
 }
